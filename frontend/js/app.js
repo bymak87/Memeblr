a9b48128328c47cf84023f4a29ed2f0941954b43
Emily Schomer <minischomer@gmail.com>
2014-05-26 15:15:36 -0500

    js updates

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,31 +8,23 @@ $(document).ready(function(){
   })
 
   $('.g-mode').on('click', function() {
-    $('#like').hide();
-    $('#g-like').show();
+    // $(this).toggleClass('#g-like');
   })
 
   $('#next').on('click', function() {
     alert('next image');
   })
 
-  $('#next').on('mouseenter', function() {
-
-  })
-
   $('#prev').on('click', function() {
     alert('prev image');
   })
 
-  $('#prev').mouseover(function(){
-    $(this).addClass('.hover');
+  $('.delete').on('click', function(){
+    alert('delete image');
   })
-
-
 
 });
 
-// finish toggle from dislike to like
 // fix g-mode to only work if liked
 // finish toggle from g-mode to regular
 // add gunshot audio on click
